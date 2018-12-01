1b39f42e681a72cd5d18c209612dccb56572df2a
Raluca <Raluca@DESKTOP-C6F1MU3>
2018-12-01 15:05:31 +0200

    started sprint 4 for UserDataServicesEJB

--- SQM4/src/main/java/org/app/service/ejb/UserDataServiceEJB.java
+++ SQM4/src/main/java/org/app/service/ejb/UserDataServiceEJB.java
@@ -6,11 +6,21 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import org.app.service.entities.User;
 
-
+@Path("user")
 @Stateless @LocalBean
 public class UserDataServiceEJB implements UserDataService{
 
@@ -30,6 +40,10 @@ public class UserDataServiceEJB implements UserDataService{
 	}
 	
 	//create or update
+	@PUT @Path("/{userName}")
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	@Override
 	public User addUser(User userToAdd) {
 		em.persist(userToAdd);
@@ -40,16 +54,24 @@ public class UserDataServiceEJB implements UserDataService{
 
 	//read
 	@Override
-	public User getUserByUsername(String userName) {
+	@GET @Path("/{userName}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public User getUserByUsername(@PathParam("userName") String userName) {
 		return em.find(User.class, userName);
 	}
 
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	public Collection<User> getUsers() {
 		List<User> users = em.createQuery("SELECT u FROM User u", User.class).getResultList();
 		return users;
 	}
 
 	//remove
+	@DELETE
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public String removeUser(User userToRemove) {
 		userToRemove = em.merge(userToRemove);
 		em.remove(userToRemove);
@@ -59,7 +81,9 @@ public class UserDataServiceEJB implements UserDataService{
 
 	//custom read
 	@Override
-	public User getUserByEmail(String email) {
+	@GET @Path("/{email}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public User getUserByEmail(@PathParam("email") String email) {
 		return em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class)
 				 .setParameter("email", email)
 				 .getSingleResult();
